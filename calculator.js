457bbd1d4682f51479a3dbef1ee54f4d8adecc17
kbly538 <kubilay.bilgili@gmail.com>
2022-10-20 04:31:29 +0300

    redo and some more clean-up

--- calculator.js
+++ calculator.js
@@ -1,7 +1,14 @@
 // Regex for validating numbers is from David Leppik's answer https://stackoverflow.com/questions/9011524/regex-to-check-whether-a-string-contains-only-numbers/30987109#30987109
 
+const IMPLEMENTATION_MESSAGE = "NOT IMPLEMENTED";
+const ERROR_DISPLAY_MESSAGE = "ERR";
+
+
 let display_value = "";
 const operators = { "÷": "divide", "×": "multiply", "-": "subtract", "+": "add" }
+const Operations = { Mod: "mod", Power: "pow", ClearAll: "AC", UndoAction: "CE" };
+const States = { FirstOperand: 1, SecondOperand: 2, Operator: 3 };
+const CalculationStatus = { NotCalculating: 1, Calculating: 2, Calculated: 3 }
 
 
 let buttons = document.querySelectorAll(".button");
@@ -13,8 +20,9 @@ function operate(operator, a, b) {
     const matchOnlyNumberRe = /NaN|-?((\d*\.\d+|\d+)([Ee][+-]?\d+)?|Infinity)$/;
     const numbersValid = a.match(matchOnlyNumberRe) != null && b.match(matchOnlyNumberRe) != null;
 
+
     if (!numbersValid) {
-        return;
+        return ERROR_DISPLAY_MESSAGE;
     }
 
 
@@ -30,9 +38,11 @@ function operate(operator, a, b) {
             return a * b;
         case "divide": {
             if (b != 0) {
-                return Math.round(a * 1000 / b * 1000) / 1000000;
+                return Math.round(a * 1000 / b * 1000) / 10000
+
             }
-            throw new Error();
+            return ERROR_DISPLAY_MESSAGE
+
 
         }
     }
@@ -44,8 +54,9 @@ let calcEntryRegExp = /[\d|.]/;
 
 
 
-let calculating = false;
-let calculated = false;
+let currentState = States.FirstOperand;
+let calculationStatus = CalculationStatus.NotCalculating;
+
 
 let num1 = "";
 let num2 = "";
@@ -55,161 +66,201 @@ let error = false;
 let operatorMarked = false;
 let answer = 0;
 
-let num1Stage = true;
-let num2Stage = false;
-let operatorStage = false;
+function checkIfNegative(num) {
+    return num[0] === "-";
+}
+
+function resetCalculation() {
+
+    num1 = "";
+    display_value = "";
+    num1 = "";
+    num2 = "";
+    operator = "";
+    floatingPoint = false;
+    error = false;
+    operatorMarked = false;
+    currentState = States.FirstOperand;
+    calculationStatus = CalculationStatus.NotCalculating;
+
+}
+
+function undoAction() {
+    let temp = display_value.slice(0, display_value.length - 1);
+    let isOP = temp[temp.length - 1] !== undefined;
+    if (isOP) {
+        currentState = States.Operator;
+    }
+    if (currentState === States.FirstOperand) {
+
+        num1 = num1.slice(0, num1.length - 1);
+
+        display_value = display_value.slice(0, display_value.length - 1);
+        if (display_value === "") {
+            resetCalculation();
+        }
+        currentState = States.FirstOperand;
+
+    } else if (currentState === States.SecondOperand) {
+
+        num2 = num2.slice(0, num2.length - 1);
+
+        display_value = display_value.slice(0, display_value.length - 1);
+
+    } else if (currentState === States.Operator) {
+        display_value = display_value.slice(0, display_value.length - 1);
+        operator = "";
+        operatorMarked = false;
+        num2 = "";
+        currentState = States.FirstOperand;
+    }
+}
+
+function processOperator(e) {
+    buttonPressed = e.target.textContent.trim();
+    if (operators[buttonPressed] === undefined) { return; }
+
+    if ((calculationStatus === CalculationStatus.Calculating)
+        && (currentState === States.Operator)
+        && !operatorMarked) {
+        operatorMarked = true;
+        currentState = States.SecondOperand;
+        operator = buttonPressed;
+        display_value += operator;
+        flip();
+    } else if (calculationStatus === CalculationStatus.NotCalculating) {
+        currentState = States.FirstOperand;
+    } else {
+        currentState = States.SecondOperand;
+    }
+
+}
+
+const btnCE = document.getElementById("CE");
+const btnAC = document.getElementById("AC");
+const btnOperators = document.querySelectorAll(".button.operator")
+
+btnCE.addEventListener("click", undoAction);
+btnAC.addEventListener("click", resetCalculation);
+btnOperators.forEach(btn => {
+    addEventListener("click", processOperator);
+});
+
+
+function flip() {
+    display.textContent = display_value;
+}
+
 
 
 buttons.forEach(b => {
     b.addEventListener("click", (e) => {
 
 
+        let buttonPressed = e.target.textContent.trim();
+        let isNumberPressed = buttonPressed.match(calcEntryRegExp);
+        if (calculationStatus === CalculationStatus.Calculated) {
+            resetCalculation();
+        }
 
-        let activeButton = e.target.textContent.trim();
-        let isNumber = activeButton.match(numbersRE);
 
 
 
 
-        if (calculated || activeButton == "AC") {
+        // Handle aux buttons
+        if (calculationStatus === CalculationStatus.NotCalculating) {
+            switch (buttonPressed) {
+                case "mod":
+                    display_value = IMPLEMENTATION_MESSAGE;
+                    break;
+                case "xy":
+                    display_value = IMPLEMENTATION_MESSAGE;
+                    break;
+                case "=":
+                    display_value = ERROR_DISPLAY_MESSAGE;
+                    break;
+                case "AC":
+                    resetCalculation(e);
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        // Handle negatives
+        let isAlreadyNegative = true;
+
+        if (buttonPressed === "-"
+            && calculationStatus === CalculationStatus.NotCalculating) {
+
             display_value = "";
-            num1 = "";
-            num2 = "";
-            operator = "";
-            floatingPoint = false;
-            error = false;
-            operatorMarked = false;
-            calculating = false;
-            calculated = false;
-        }
-
-        if (((num1 === "") && (!isNumber && (activeButton != "AC" && activeButton != "-")))) {
-            if (activeButton === "mod") {
-                display_value = "NOT IMPLEMENTED"
-            } else if (activeButton === "xy" || activeButton === "y") {
-                display_value = "NOT IMPLEMENTED";
-            } else if ((num1 === "" || num2 === "") && activeButton === "=") {
-                display_value = display_value;
+            isAlreadyNegative = checkIfNegative(num1);
+            if (isAlreadyNegative) {
+                return;
             }
-            else {
-                display_value = "ERROR"
-                error = true;
-            }
-
-            calculated = true;
-        } else if ((num1 === "") && activeButton == "-") {
             num1 += "-";
+            display_value += "-";
+
+
+        }
+        else if (buttonPressed === "-"
+            && currentState === States.SecondOperand
+            && num2 === "") {
+            isAlreadyNegative = checkIfNegative(num2);
+            if (isAlreadyNegative) {
+                return;
+            }
+            num2 += "-";
+            display_value += "-";
         }
 
 
+        // Handle Operands
+        if (currentState === States.FirstOperand
+            && isNumberPressed) {
 
-        if (num1 !== "" && !isNumber) {
-            if (operators[activeButton] != undefined && !operatorMarked) {
-
-
-                if (num1 !== "-") {
-                    operatorMarked = true;
-                    display_value += activeButton;
-                    operator = operators[activeButton];
-                    calculating = true;
-                    floatingPoint = false;
-                    operatorStage = true;
-                    num1Stage = false;
-                    operatorStage = true;
-                    num2Stage = false;
-                } else {
-                    display_value += activeButton;
-                    operator = operators[activeButton];
-                    floatingPoint = false;
-
-                }
-            } else if (num2 !== "" && activeButton == "=") {
-                calculated = true;
-                calculating = false;
-                display_value = operate(operator, num1, num2);
-                num1Stage = false;
-                num2Stage = false;
-                operatorStage = false;
+                if (num1.length === 0 && buttonPressed === ".")
+            {
+                buttonPressed = "0."
             }
-        } else {
-            if (!calculating && (isNumber || activeButton == ".") && !error) {
-                num1Stage = true;
-                if (activeButton == "." && floatingPoint) {
-                    return;
-                } else if (activeButton == ".") {
-                    floatingPoint = true;
-                }
-                calculated = false;
-                if (num1.length < 8 || operators[activeButton] === "-") {
-                    num1 += activeButton;
-                    display_value += activeButton;
-                    num2Stage = false;
-                    operatorStage = false;
 
-                } else {
-                    alert("Cannot be larger than this.")
-                }
-            } else if (calculating && isNumber || activeButton == "." && !error) {
-                num2Stage = true;
-                if (activeButton == "." && floatingPoint) {
-                    return;
-                } else if (activeButton == ".") {
-                    floatingPoint = true;
-                }
-                if (num2.length < 8) {
-                    num2 += activeButton;
-                    display_value += activeButton;
-                    num1Stage = false;
-                    operatorStage = false;
+            currentState = States.FirstOperand
+            calculationStatus = CalculationStatus.Calculating;
+            display_value += buttonPressed
+            num1 += buttonPressed;
 
-                }
+
+        } else if ((currentState === States.SecondOperand
+            && isNumberPressed)) {
+            
+            if (num2.length === 0 && buttonPressed === ".")
+            {
+                buttonPressed = "0."
             }
+                
+            currentState = States.SecondOperand;
+            calculationStatus = CalculationStatus.Calculating;
+            display_value += buttonPressed
+            num2 += buttonPressed;
+
+
+        } else if (operators[buttonPressed] !== undefined && calculationStatus === CalculationStatus.Calculating) {
+            currentState = States.Operator;
+
         }
 
 
-        if (activeButton === "CE") {
-            if (num1Stage) {
-                console.log("before1: " + display_value)
-                num1 = num1.slice(0, num1.length - 1);
-                display_value = display_value.slice(0, display_value.length - 1);
-                console.log("after1: " + display_value)
-                num2Stage = false;
-                operatorStage = false;
-                num1Stage = true;
-
-
-            } else if (num2Stage) {
-                console.log("before2: " + display_value)
-                num2 = num2.slice(0, num2.length - 1);
-                console.log("after2" + display_value)
-                display_value = display_value.slice(0, display_value.length - 1);
-                if (display_value != isNumber) {
-                    operatorStage = true;
-                    num2Stage = false;
-
-                } else {
-                    operatorStage = false;
-                }
-            } else if (operatorStage) {
-                console.log("beforeop: " + display_value)
-                operator = "";
-                calculating = false;
-                num2 = "";
-                num2Stage = false;
-                operatorMarked = false;
-                num1Stage = true;
-                display_value = display_value.slice(0, display_value.length - 1);
-                operatorStage = true;
-                console.log("after: " + display_value)
-            } else {
-                calculated = true;
-                display_value = "0";
-                num1Stage = true;
-            }
+        // Handle result
+        if (num2 !== "" && buttonPressed === "=") {
+            answer = operate(operators[operator], num1, num2);
+            display_value = "" + answer;
+            flip();
+            calculationStatus = CalculationStatus.Calculated;
         }
 
-        display.textContent = display_value;
-        
-
+        flip();
     });
+
 })
